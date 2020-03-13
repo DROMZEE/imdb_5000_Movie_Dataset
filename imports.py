33c225950f4b6ea5b99d2eb82685ef289af8042f
DROMZEE CEDRIC <cedric@dromzee.fr>
2020-03-13 01:10:36 +0100

    addition of the library import file for preprocessing notebook

--- imports.py
+++ imports.py
@@ -3,4 +3,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import missingno as msno
+
+from pandas_profiling import ProfileReport
+import sys
+
+from sklearn.decomposition import PCA
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cluster import KMeans
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+
+
+import datetime
+import time
+from time import strftime
+from time import gmtime
+
+%matplotlib inline
+plt.style.use("seaborn")
+
 plt.rcParams["figure.figsize"] = (16,10)
+pd.set_option('display.max_columns', None)
